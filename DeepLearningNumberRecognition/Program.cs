446a21d62e2a2e970b8e62017e64805e77cb939b
Akihito759 <GregCudok@yahoo.co.uk>
2018-01-05 15:57:09 +0100

    Ostateczne poprawki

--- DeepLearningNumberRecognition/Program.cs
+++ DeepLearningNumberRecognition/Program.cs
@@ -17,13 +17,13 @@ namespace DeepLearningNumberRecognition
             var sw = new Stopwatch();
             sw.Start();
             var reader = new MNISTReader();
-            //reader.ConsolePrintImage(reader.TestImagesArray, reader.TestImagesLabel, true);
+            //reader.ConsolePrintImage(reader.TestImagesArray, reader.TestImagesLabel, true); //Wypisywanie obrazków 
             var trainingAdapeter = new MistToNeutralNetworkAdapter(reader.TrainingImagesArray, reader.TrainingImagesLabel, 1000);
             var testAdapter = new MistToNeutralNetworkAdapter(reader.TestImagesArray, reader.TestImagesLabel, 10000);
             var DDP = new NeutralNetwork();
-            DDP.LoadNetwork("Network60k1k");
+            DDP.LoadNetwork("Network60k1k"); //wczytranie przetrenowanej sieci neuronowej
             var ImageReaader = new ImageReader();
-            //DDP.TrainNetwork(trainingAdapeter.ImagesVectorArray, trainingAdapeter.LabelsArray);
+            //DDP.TrainNetwork(trainingAdapeter.ImagesVectorArray, trainingAdapeter.LabelsArray); funkcja do terenowania sieci neuronwoej 
             DDP.ComputeNetwork(testAdapter.ImagesVectorArray, testAdapter.LabelsArray);
             DDP.ComputeNetwork(ImageReaader.ImagesArray);
             sw.Stop();
