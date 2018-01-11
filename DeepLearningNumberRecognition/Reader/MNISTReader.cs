743cb83b81a08dea6aa3058e6b21884648443015
Akihito759 <GregCudok@yahoo.co.uk>
2018-01-11 08:01:10 +0100

    .

--- DeepLearningNumberRecognition/Reader/MNISTReader.cs
+++ DeepLearningNumberRecognition/Reader/MNISTReader.cs
@@ -167,6 +167,7 @@ namespace DeepLearningNumberRecognition
                         else
                         {
                             writer.Append("."); //grey
+
                         }
 
                       
